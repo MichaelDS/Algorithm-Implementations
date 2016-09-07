489620b7ba02a746df2d5780a9d20123cf9b60df
MichaelDS <mdsalerno@gmail.com>
2016-09-07 08:35:21 -0400

    Modularized some heap operations

--- 7. Data Structures/heap.py
+++ 7. Data Structures/heap.py
@@ -3,6 +3,7 @@ from math import floor
 class heap(object):
     '''
     A heap.
+    #TODO: Modularize bubbling
     '''
     def __init__(self):
         self.elements = []
@@ -10,7 +11,16 @@ class heap(object):
         return len(self.elements)
     def insert(self, x):
         self.elements.append(x)
-        index = len(self.elements)
+        self.bubble_up(index = len(self.elements))
+    def extract(self):
+        if self.size() == 0:
+            raise IndexError('extract from empty heap')
+        self.elements[0], self.elements[self.size()-1] = \
+        self.elements[self.size()-1], self.elements[0]
+        minimum = self.elements.pop()
+        self.bubble_down(index = 1)
+        return minimum
+    def bubble_up(self, index):
         while index > 1:
             parent = floor(index/2)
             if self.elements[parent-1] <= self.elements[index-1]:
@@ -18,17 +28,12 @@ class heap(object):
             self.elements[parent-1], self.elements[index-1] = \
             self.elements[index-1], self.elements[parent-1]
             index = parent
-    def extract(self):
-        if self.size() == 0:
-            raise IndexError('extract from empty heap')
-        self.elements[0], self.elements[self.size()-1] = \
-        self.elements[self.size()-1], self.elements[0]
-        minimum = self.elements.pop()
-        index = 1
+    def bubble_down(self, index):
         left = 2*index
         right = 2*index + 1
         while left <= self.size():
-            if right > self.size() or self.elements[left-1] <= self.elements[right-1]:
+            if right > self.size() or \
+            self.elements[left-1] <= self.elements[right-1]:
                 min_child = left
             else:
                 min_child = right
@@ -39,4 +44,3 @@ class heap(object):
             index = min_child
             left = 2*index
             right = 2*index + 1
-        return minimum
